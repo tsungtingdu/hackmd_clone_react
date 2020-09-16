08ae9ab43526e34e34ef75dcca49ffdca51b6862
tsungtingdu <tsungtingdu@gmail.com>
2020-09-16 11:37:15 +0800

    add icon on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 import { withRouter } from 'react-router-dom'
+import docs from '../images/docs.png'
 
 const Nav = styled.div`
   width: 100%;
@@ -15,11 +16,17 @@ const Nav = styled.div`
     height: 20px;
     color: #ffffff;
     font-weight: 900;
-
+    display: flex;
+    align-content: center;
+    cursor: pointer;
     &_title {
       font-size: 20px;
-      line-height: 20px;
-      cursor: pointer;
+      line-height: 20px; 
+    }
+    &_img {
+      width: 20px;
+      height: 20px;
+      margin-right: 5px;
     }
   }
 `
@@ -37,8 +44,9 @@ const Navbar = (props) => {
   }
   return (
     <Nav>
-      <div className="nav_content">
-        <div className="nav_content_title" onClick={handleClick}>HeyMD</div>
+      <div className="nav_content" onClick={handleClick}>
+        <img className="nav_content_img" src={docs} />
+        <div className="nav_content_title">HeyMD</div>
       </div>
     </Nav>
   )
