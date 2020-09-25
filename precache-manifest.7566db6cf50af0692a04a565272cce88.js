9ff74707c7635184ecfd5db370eb2c36ab7775df
tsungtingdu <tsungtingdu@gmail.com>
2020-09-25 14:42:28 +0800

    Updates

--- precache-manifest.7566db6cf50af0692a04a565272cce88.js
+++ precache-manifest.7566db6cf50af0692a04a565272cce88.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b21d948e2f1a98b8d8fb270c23ce2ac",
+    "revision": "f2bfde911845c14503729dec4eade01d",
     "url": "/heymd/index.html"
   },
   {
-    "revision": "641ce47d3dca76352f18",
+    "revision": "efe2dadd01eb42da226e",
     "url": "/heymd/static/css/main.ef7712fe.chunk.css"
   },
   {
-    "revision": "dfbbb063c2c22a543cff",
-    "url": "/heymd/static/js/2.444cf32f.chunk.js"
+    "revision": "b614b3395e28f1739a38",
+    "url": "/heymd/static/js/2.e43f51e9.chunk.js"
   },
   {
     "revision": "79abd7d95989c19183a513be5dc2ab88",
-    "url": "/heymd/static/js/2.444cf32f.chunk.js.LICENSE.txt"
+    "url": "/heymd/static/js/2.e43f51e9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "641ce47d3dca76352f18",
-    "url": "/heymd/static/js/main.ad327f25.chunk.js"
+    "revision": "efe2dadd01eb42da226e",
+    "url": "/heymd/static/js/main.0fed02d7.chunk.js"
   },
   {
     "revision": "9e8052052a1f54c6de96",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/heymd/precache-manifest.12127008b59b1dbbb4244b140ef66977.js"
+  "/heymd/precache-manifest.7566db6cf50af0692a04a565272cce88.js"
 );
 
 self.addEventListener('message', (event) => {
