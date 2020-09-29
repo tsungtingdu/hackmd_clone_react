e2070da06300665972a02f847434c51dee9d6926
tsungtingdu <tsungtingdu@gmail.com>
2020-09-29 14:22:45 +0800

    install toastify and create customised style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import Router from "./Router";
 import "./css/index.scss";
 import configStore from "./store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components";
 
 const store = configStore();
 
-ReactDOM.render(<Router store={store} />, document.getElementById("root"));
+const StyledToastContainer = styled.div`
+  .Toastify__toast {
+    border-radius: 4px;
+    padding: 15px;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+    &-body {
+      font-family: "Source Sans Pro", sans-serif;
+      font-weight: 700;
+      margin: 0;
+      display: flex;
+      align-items: center;
+    }
+    &--success {
+      background-color: #5cb85c;
+    }
+    &--error {
+      background-color: #c9302c;
+    }
+  }
+`;
+
+ReactDOM.render(
+  <Fragment>
+    <StyledToastContainer>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </StyledToastContainer>
+    <Router store={store} />
+  </Fragment>,
+  document.getElementById("root")
+);
