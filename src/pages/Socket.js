200d9950ad3044fafba0a688eb1639a893db2ea4
tsungtingdu <tsungtingdu@gmail.com>
2020-09-30 18:12:23 +0800

    set caret position with local input position

--- src/pages/Socket.js
+++ src/pages/Socket.js
@@ -47,18 +47,40 @@ const SocketPage = () => {
   const [input, setInput] = useState();
   const dispatch = useDispatch();
 
+  const getCaretPostion = () => {
+    const myElement = document.querySelector("textarea");
+    let startPosition = myElement.selectionStart;
+    localStorage.setItem("caret", startPosition);
+  };
+
+  const setCaretPostion = () => {
+    const myElement = document.querySelector("textarea");
+    myElement.focus();
+    let caret = localStorage.getItem("caret");
+    caret = caret ? Number(caret) : null;
+    myElement.setSelectionRange(caret, caret);
+  };
+
   const handleChange = (e) => {
     setInput(e);
     saveToLocal({ id: roomId, content: input });
-
+    // get and set local real-time caret position
+    getCaretPostion();
+    setCaretPostion();
     // socket, emit message
     socket.emit("post", roomId, e);
   };
 
   const handleKeyPress = (e) => {
+    getCaretPostion();
     if (e.key === "Enter") {
       setInput(input + " " + "\n" + " ");
     }
+  };
+
+  const handleClick = (e) => {
+    // get local real-time caret position
+    getCaretPostion();
   };
 
   // set socket & get initial post data
@@ -82,6 +104,8 @@ const SocketPage = () => {
   const syncMessage = useRef(
     _.debounce((data) => {
       setInput(data.msg);
+      // set caret back to local position
+      setCaretPostion();
     }, 600)
   ).current;
 
@@ -101,13 +125,14 @@ const SocketPage = () => {
       });
       return () => {
         socket.close();
+        localStorage.setItem("caret", 0);
       };
     }
   }, [socket]);
 
   return (
     <Container>
-      <EditorContainer onKeyPress={handleKeyPress}>
+      <EditorContainer onKeyPress={handleKeyPress} onClick={handleClick}>
         <Editor
           value={input}
           height="calc(100vh - 50px)"
